1f3a579b1745145876a0b7b8cdcc6cbfaf9f2539
ifan <ifanabedin034@gmail.com>
2025-04-09 00:48:45 +0600

    hash router added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
@@ -11,9 +11,9 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
