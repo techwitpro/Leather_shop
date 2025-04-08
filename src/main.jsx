a97cd09ce6e203b4e760c059ad7ff65c6fd3bc5d
ifan <ifanabedin034@gmail.com>
2025-04-09 01:04:15 +0600

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
@@ -11,9 +11,9 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
 
