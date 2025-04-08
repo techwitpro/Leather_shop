d99f0390fce5f8b79959e3fe2c4c0f3822be314a
ifan <ifanabedin034@gmail.com>
2025-04-09 03:14:49 +0600

    error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Layouts from "./Layouts/MainLayout";
+import MainLayout from "./layouts/MainLayout";  // Correct the import path
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
 import Shop from "./Components/Shop/Shop";
@@ -8,13 +8,15 @@ import Shop from "./Components/Shop/Shop";
 function App() {
   return (
     <Routes>
-      <Route element={<Layouts />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/shop" element={<Shop />} />
+      {/* MainLayout wraps all routes */}
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />  {/* Default route */}
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
 }
+
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -13,3 +13,5 @@ export default function MainLayout() {
     </div>
   )
 }
+
+
